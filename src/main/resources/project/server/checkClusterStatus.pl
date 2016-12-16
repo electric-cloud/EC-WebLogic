e80205b9848fdffbe46f71b81dc8dbb36559fc80
justnoxx <justnoxx@gmail.com>
2016-12-16 17:10:42 +0200

    Finished check cluster status procedure.

--- src/main/resources/project/server/checkClusterStatus.pl
+++ src/main/resources/project/server/checkClusterStatus.pl
@@ -31,13 +31,13 @@ sub main {
     my $wl = EC::WebLogic->new(
         project_name => $PROJECT_NAME
     );
-    my $params = $wl->get_params_as_hashref(
-        'wlstabspath',
-        'configname',
-        'clustername',
-        'successcriteria',
-        'maxelapsedtime'
-    );
+    my $params = $wl->get_params_as_hashref(qw{
+        wlstabspath
+        configname
+        clustername
+        successcriteria
+        maxelapsedtime
+    });
 
     $wl->out(1, "Executable file: ", $params->{wlstabspath});
     my $check = $wl->check_executable($params->{wlstabspath});
@@ -45,97 +45,35 @@ sub main {
     unless ($check->{ok}) {
         $wl->bail_out($check->{msg});
     }
-    $params->{wlstabspath} = $wl->esc_args($params->{wlstabspath});
     my $config_name = $params->{configname};
     my $cred = $wl->get_credentials($config_name);
-    if ($cred->{java_home}) {
-        $ENV{JAVA_HOME} = $cred->{java_home};
+
+    my $maxelapsedtime = 0;
+    if ($params->{maxelapsedtime}) {
+        if ($params->{maxelapsedtime} !~ m/^[0-9]+$/s) {
+            $wl->bail_out("Max elapsed time should be a positive integer.");
+        }
+        $maxelapsedtime = $params->{maxelapsedtime};
     }
-    print Dumper $cred;
-    exit 0;
+    $params->{successcriteria} = $params->{successcriteria} eq 'RUNNING' ? 'RUNNING' : 'SHUTDOWN';
+    my $render_params = {
+        username => $cred->{user},
+        password => $cred->{password},
+        admin_url => $cred->{weblogic_url},
+        maxelapsedtime => $maxelapsedtime,
+        cluster_name => $params->{clustername},
+        success_criteria => $params->{successcriteria}
+    };
+    my $template_path = '/myProject/jython/check_cluster_status.jython';
+    my $template = $wl->render_template_from_property($template_path, $render_params);
+    $wl->out(10, "Generated script:\n", $template);
 
+    my $res = $wl->execute_jython_script(
+        shell => $params->{wlstabspath},
+        script_path => $ENV{COMMANDER_WORKSPACE} . '/exec.jython',
+        script_content => $template,
+    );
+
+    $wl->process_response($res);
+    return;
 }
-#     my $render_params = {
-#         username => $cred->{user},
-#         password => $cred->{password},
-#         servername => $params->{instancename},
-#         admin_url => $cred->{weblogic_url}
-#     };
-#     my $template_path = '/myProject/jython/check_server_status.jython';
-
-#     my $template = $wl->render_template_from_property($template_path, $render_params);
-
-#     $wl->out(10, "Generated script:\n", $template);
-
-#     my $path = $wl->generate_exec_path();
-#     open FH, '>', $path;
-#     print FH $template;
-#     close FH;
-
-#     my $exec_result;
-#     $path = $wl->esc_args($path);
-#     my $cmd = "$params->{wlstabspath} $path";
-#     if ($params->{maxelapsedtime} && $params->{maxelapsedtime} > 0) {
-#         $exec_result = $wl->do_while(
-#             sub {
-#                 return check_server_status($wl, $params, $cmd);
-#             },
-#             1,
-#             $params->{maxelapsedtime}
-#         );
-#     }
-#     else {
-#         $exec_result = check_server_status($wl, $params, $cmd);
-#     }
-
-#     unlink $path;
-
-#     if ($exec_result > 0) {
-#         $wl->out(1, "Criteria was met");
-#         $wl->success();
-#         return;
-#     }
-
-#     $wl->out(1, "Criteria wasn't met");
-#     $wl->error();
-#     return;
-# };
-
-# sub check_server_status {
-#     my ($wl, $params, $cmd) = @_;
-
-#     $wl->out(1, '=' x 60);
-#     $wl->out(1, "Checking server status");
-#     my $result = $wl->run_command($cmd);
-
-#     if ($result->{stderr} =~ m/^TIMEOUT\n$/s) {
-#         return -2;
-#     }
-#     $wl->out(1, "Success criteria: ", $params->{successcriteria});
-#     my ($criteria, $server_running) = (0, 0);
-#     $criteria = $params->{successcriteria} eq 'RUNNING' ? 1 : 0;
-
-#     $wl->out(1, "EXIT_CODE:", $result->{code});
-#     $wl->out(1, "STDOUT: ", $result->{stdout});
-#     $wl->out(1, "STDERR: ", $result->{stderr});
-
-#     if ($result->{stdout} =~ m/Server\sState:NO_SERVER_FOUND/s) {
-#         # error
-#         # $wl->error("NO_SERVER_FOUND");
-#         $wl->out(1, "NO_SERVER_FOUND");
-#         return -1;
-#     }
-
-#     if ($result->{stdout} =~ m/Server\sState:\sRUNNING/s) {
-#         $server_running = 1;
-#     }
-
-#     if ($criteria == $server_running) {
-#         # Success, criteria was met
-#         return 1;
-#     }
-
-#     # criteria wasn't met
-#     return 0;
-# }
-
