331f5d0fb9128226bf361d60fc38e4aa3098a1f4
Dmitriy Shamatrin <justnoxx@gmail.com>
2018-05-14 14:07:51 +0300

    Revert "Re-merge of PoC changes"

--- src/main/resources/project/conf/createAndAttachCredential.pl
+++ src/main/resources/project/conf/createAndAttachCredential.pl
@@ -194,17 +194,6 @@ $xpath = $ec->attachCredential($projName, $credName,
      stepName => 'CheckClusterStatus'});
 $errors .= $ec->checkAllErrors($xpath);
 
-$xpath = $ec->attachCredential($projName, $credName,
-    {procedureName => 'CreateOrUpdateJMSResource',
-     stepName => 'CreateOrUpdateJMSResource'});
-$errors .= $ec->checkAllErrors($xpath);
-
-$xpath = $ec->attachCredential($projName, $credName,
-    {procedureName => 'CreateOrUpdateDatasource',
-     stepName => 'CreateOrUpdateDatasource'});
-$errors .= $ec->checkAllErrors($xpath);
-
-
 if ($errors ne '') {
 
     # Cleanup the partially created configuration we just created
