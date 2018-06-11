4191f9fc4eb14bfb19540cfef17fa703170ea9a7
Anton Horodchuk <antoman1994@gmail.com>
2018-06-11 20:05:01 +0300

    Fixes StartAppSuite

--- specs/StartAppSuite.groovy
+++ specs/StartAppSuite.groovy
@@ -68,11 +68,11 @@ class StartAppSuite extends WebLogicHelper {
     // Procedure params
     def configname
     def wlstabspath
-    def appname
+    def appName
 
     // Optional parameters
-    def additional_options
-    def version_identifier
+    def additionalOptions
+    def versionIdentifier
 
     // expected results
     def expectedOutcome
@@ -92,19 +92,17 @@ class StartAppSuite extends WebLogicHelper {
         setupResource()
         createConfig(CONFIG_NAME)
 
-        publishArtifact(artifactName, version, FILENAME)
-        def path = downloadArtifact(artifactName, getResourceName())
-        assert path
-
         def deploy = deployApplication(projectName,
             [
                 configname : CONFIG_NAME,
                 wlstabspath: getWlstPath(),
                 appname    : APPLICATION_NAME,
-                apppath    : path,
+                apppath    : '', // This will be filled by downloadArtifact()
                 targets    : 'AdminServer',
-                is_library : ""
-            ]
+                is_library : "false"
+            ],
+            artifactName,
+            FILENAME
         )
 
         assert deploy.outcome == 'success'
@@ -127,11 +125,11 @@ class StartAppSuite extends WebLogicHelper {
             procedureName: procedureName,
             params       : [
                 configname        : CONFIG_NAME,
-                wlstabspath       : wlstabspath,
-                appname           : appname,
+                wlstabspath       : '',
+                appname           : '',
 
-                additional_options: additional_options,
-                version_identifier: version_identifier
+                additional_options: '',
+                version_identifier: '',
             ]
         ]
     }
@@ -150,14 +148,14 @@ class StartAppSuite extends WebLogicHelper {
 
     @Unroll
     //Positive Scenarios for delete should be first
-    def "Start Application. appname '#appname' - #expectedOutcome : #expectedSummaryMessage"() {
+    def "Start Application. appname '#appName' - Expected: #expectedOutcome : #expectedSummaryMessage"() {
         setup: 'Define the parameters for Procedure running'
         def runParams = [
             wlstabspath       : wlstabspath,
-            appname           : appname,
+            appname           : appName,
 
-            additional_options: additional_options,
-            version_identifier: version_identifier
+            additional_options: additionalOptions,
+            version_identifier: versionIdentifier
         ]
 
         when: 'Procedure runs: '
@@ -174,6 +172,7 @@ class StartAppSuite extends WebLogicHelper {
             def pageAfterStart = checkUrl(APPLICATION_PAGE_URL)
             assert pageAfterStart.code == SUCCESS_RESPONSE
         }
+
         if (expectedSummaryMessage) {
             def upperStepSummary = getJobUpperStepSummary(result.jobId)
             assert upperStepSummary.contains(expectedSummaryMessage)
@@ -183,7 +182,7 @@ class StartAppSuite extends WebLogicHelper {
         if (expectedOutcome == expectedOutcomes.success && result.outcome == expectedOutcomes.success) {
             stopApplication(projectName, [
                 configname        : CONFIG_NAME,
-                appname           : appname,
+                appname           : appName,
                 wlstabspath       : wlstabspath,
 
                 additional_options: "",
@@ -192,7 +191,7 @@ class StartAppSuite extends WebLogicHelper {
         }
 
         where: 'The following params will be: '
-        wlstabspath | appname          | additional_options | version_identifier | expectedOutcome          | expectedSummaryMessage
-        wlstPath    | APPLICATION_NAME | ''                 | ''                 | expectedOutcomes.success | ''
+        wlstabspath | appName          | additionalOptions | versionIdentifier | expectedOutcome          | expectedSummaryMessage
+        wlstPath    | APPLICATION_NAME | ''                | ''                | expectedOutcomes.success | ''
     }
 }
